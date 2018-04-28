aab80f29a9c736f5741f9f1d796c43a71465d2f4
Ignasius Leonardo <ignasiusleonardo@gmail.com>
2018-04-29 02:38:12 +0700

    Update2

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -16,13 +16,13 @@ class CheckRole
     public function handle($request, Closure $next)
     {
         if ($request->user() === null) {
-            return response("Insufficient permissions", 401);
+            return response("Insufficient Permissions", 401);
         }
         $actions = $request->route()->getAction();
         $roles = isset($actions['roles']) ? $actions['roles'] : null;
         if ($request->user()->hasAnyRole($roles) || !$roles) {
             return $next($request);
         }
-        return response("Insufficient permissions", 401);
+        return redirect()->back()->with('messageFromAdmin','Insufficient Permissions');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('content')
+    @if(Session::has('messageFromAdmin'))
+        <div class="alert {{ Session::get('alert alert-danger', 'alert-danger') }}">{{ Session::get('messageFromAdmin') }}</div>
+    @endif
 <h1>Welcome to dashboard</h1>
 @endsection
 
