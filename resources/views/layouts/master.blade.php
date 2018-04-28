94089f8dbd7c3bea73b5c5aa9dbffa41b96fe228
Ignasius Leonardo <ignasiusleonardo@gmail.com>
2018-04-29 02:06:40 +0700

    Update2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,9 +39,7 @@
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="/admin"><i class="fa fa-user fa-fw"></i> Admin Menu</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        <li><a href="/admin"><i class="fa fa-gear fa-fw"></i>UIAdmin Settings</a>
                         </li>
                         <li class="divider"></li>
                         <li><a href="/logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
