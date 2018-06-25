343ba990f32fa48477e543ca44cd0284055384af
Ignasius Leonardo <ignasiusleonardo@gmail.com>
2018-06-25 07:35:11 +0700

    Update3.6

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -17,7 +17,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $students=Student::paginate(4);
+        $students=Student::paginate(3);
         return view('student.home',['students'=>$students]);
     }
 
