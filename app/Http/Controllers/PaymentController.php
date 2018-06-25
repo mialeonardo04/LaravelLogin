5c155ba9b281a8d93154ec77d3e6fb7a87328fa7
Ignasius Leonardo <ignasiusleonardo@gmail.com>
2018-06-26 01:33:28 +0700

    Update3.6

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 use App\Payment;
 use App\Pembayaran;
 use DB;
@@ -39,7 +40,10 @@ class PaymentController extends Controller
      */
     public function create()
     {
-        return view('payment.create');
+        $studentNis = Student::select('NIS','Nama')->get();
+        return view('payment.create',[
+            'nis' => $studentNis
+        ]);
     }
 
     /**
